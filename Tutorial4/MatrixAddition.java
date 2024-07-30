d4d23a3bc10e5584005c8253b580d8cf886246bd
Emma-KXA <159710400+Emmanuel-BG262@users.noreply.github.com>
2024-07-30 21:35:46 +0530

    Create MatrixAddition.java

---
+++ Tutorial4/MatrixAddition.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class MatrixAddition {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows and columns for both the matrix(rows, columns):");
+        int row = sc.nextInt();
+        int clmn= sc.nextInt();
+        int[][] Mat = new int[row][clmn];
+        int[][] Mat1 = new int[row][clmn];
+        System.out.println("\nEnter the elements to the first matrix:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < clmn; j++) {
+                System.out.printf("Matrix[%d][%d]:",i,j);
+                Matrix[i][j] = sc.nextInt();   
+            }
+        }
+        System.out.println("\nEnter elements to matrix2:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < clmn; j++) {
+                System.out.printf("MatrixB[%d][%d]:",i,j);
+                MatrixB[i][j] = sc.nextInt();   
+            }
+        }
+        System.out.println("\nMatrixA is:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < clmn; j++) {
+                System.out.print(Matrix[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println("\nMatrixB is:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < clmn; j++) {
+                System.out.print(MatrixB[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        int[][] MatrixC = new int[row][clmn];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < clmn; j++) {
+                MatrixC[i][j] = Matrix[i][j] + MatrixB[i][j];
+            }
+        }
+        System.out.println("\nMatrixA + MatrixB is:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < clmn; j++) {
+                System.out.print(MatrixC[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
